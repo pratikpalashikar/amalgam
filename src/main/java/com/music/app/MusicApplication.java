c6cff634a24ac8e01118dcdec6034f65e43db294
PratikPalashikar <pratikpalashikar@gmail.com>
2018-12-17 11:23:00 -0500

    connecting to cassanra

--- src/main/java/com/music/app/MusicApplication.java
+++ src/main/java/com/music/app/MusicApplication.java
@@ -1,9 +1,9 @@
 package com.music.app;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
--- src/main/java/com/music/app/config/CassandraConfig.java
+++ src/main/java/com/music/app/config/CassandraConfig.java
@@ -2,8 +2,15 @@ package com.music.app.config;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
+import org.springframework.data.cassandra.core.mapping.BasicCassandraMappingContext;
+import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
 
+
+@Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
 
@@ -16,7 +23,7 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     private String ip;
 
     @Value("${spring.data.port}")
-    private String port;
+    private int port;
 
 
     @Override
@@ -24,6 +31,23 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
         return keySpaceName;
     }
 
+
+    @Bean
+    public CassandraClusterFactoryBean cluster(){
+
+       CassandraClusterFactoryBean cassandraClusterFactoryBean = new CassandraClusterFactoryBean();
+       cassandraClusterFactoryBean.setContactPoints(ip);
+       cassandraClusterFactoryBean.setPort(port);
+
+       return cassandraClusterFactoryBean;
+
+    }
+
+
+    @Bean
+    public CassandraMappingContext cassandraMapping() {
+        return new BasicCassandraMappingContext();
+    }
 
 
 
