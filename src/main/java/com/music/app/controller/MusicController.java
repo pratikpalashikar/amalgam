d7e5d9578484e6ad283d774a8f76cf248041faeb
PratikPalashikar <pratikpalashikar@gmail.com>
2018-12-29 01:07:35 -0500

    Completing the upload the song functionality

--- src/main/java/com/music/app/controller/MusicController.java
+++ src/main/java/com/music/app/controller/MusicController.java
@@ -4,12 +4,13 @@ import com.music.app.model.MetaDataTable;
 import com.music.app.model.MetaDataTableID;
 import com.music.app.services.MusicRetriever;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.List;
+
 
 @RestController
 public class MusicController {
@@ -26,11 +27,46 @@ public class MusicController {
 
     }
 
-
+    /*
+    *
+    * "song_name": "Ankh Marey",
+	"genre":"pop",
+	"album_name":"Tere mere sapney",
+	"artist_name":"Arshad warsi",
+	"writer":"Anand Bakshi",
+	"publisher": "T-series"
+    *
+    * */
 
     @RequestMapping(value = "/uploadsong", method = RequestMethod.POST )
-    public MetaDataTableID uploadSong(@RequestBody MetaDataTable metaDataTable){
-        return musicRetriever.uploadSong(metaDataTable);
+    public MetaDataTableID uploadSong(
+            @RequestParam("file") MultipartFile uploadfile,
+            @RequestParam("song_name") String songName,
+            @RequestParam("genre") String genre,
+            @RequestParam("album_name") String albumName,
+            @RequestParam("artist_name") String artistName,
+            @RequestParam("writer") String writer,
+            @RequestParam("publisher") String publisher
+            ){
+
+
+        MetaDataTable metaDataTable = new MetaDataTable();
+        metaDataTable.setAlbum_name(albumName);
+        metaDataTable.setArtist_name(artistName);
+        metaDataTable.setGenre(genre);
+        metaDataTable.setPublisher(publisher);
+        metaDataTable.setSong_name(songName);
+        metaDataTable.setWriter(writer);
+
+        MetaDataTableID metaDataTableID = new MetaDataTableID();
+
+        try {
+            metaDataTableID.setData(ByteBuffer.wrap(uploadfile.getBytes()));
+        }catch (IOException e){
+            e.printStackTrace();
+
+        }
+        return musicRetriever.uploadSong(metaDataTable,metaDataTableID);
     }
 
 
